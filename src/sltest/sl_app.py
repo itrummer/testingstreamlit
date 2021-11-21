5beddc3f55df806f477a960bebade3f7242c4246
itrummer <immanuel.trummer@gmail.com>
2021-11-20 21:38:32 -0500

    Added html start page.

--- src/sltest/sl_app.py
+++ src/sltest/sl_app.py
@@ -8,3 +8,10 @@ import streamlit as st
 st.header('Hello!')
 if st.button('Balloons?'):
     st.balloons()
+
+import firebase_admin
+from firebase_admin import credentials
+
+import firebase_admin
+
+cred = credentials.Certificate
